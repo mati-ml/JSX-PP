30a6d844b343d55e4be3a72f27c086f1540a2540
Matias Muller <mmullerlanas@gmail.com>
2024-06-04 01:55:15 -0400

    armado

--- proyecto-final/src/cookieUtils.js
+++
@@ -1,6 +0,0 @@
-// cookieUtils.js
-export const getCookie = (name) => {
-    const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
-    return cookieValue ? cookieValue.pop() : null;
-  };
-  
