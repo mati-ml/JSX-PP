d3ce60e5ab9f68349d1c2d7821fd9a2374a8a4f1
Matias Muller <mmullerlanas@gmail.com>
2024-06-06 01:15:01 -0400

    Mini arreglo en eval models.py

--- proyecto-final/api/authtutorial/evaluation/models.py
+++ proyecto-final/api/authtutorial/evaluation/models.py
@@ -4,7 +4,7 @@ from users.models import User
 class Eval(models.Model):
     user_id = models.IntegerField()
     user_email = models.EmailField()
-    user_role=rut_emp = models.CharField(max_length=50, null=True, blank=True)
+    user_role= models.CharField(max_length=50, null=True, blank=True)
     estado = models.CharField(max_length=20)  # Terminado o Pendiente
     
     # Nuevas columnas
