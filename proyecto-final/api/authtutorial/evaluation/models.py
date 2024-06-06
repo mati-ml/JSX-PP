cc0a2bd2a39ba0e418098c4cf166ce75e88be0ba
Matias Muller <mmullerlanas@gmail.com>
2024-06-06 00:54:38 -0400

    Problemas de agregar nuevas columnas arreglado

--- proyecto-final/api/authtutorial/evaluation/models.py
+++ proyecto-final/api/authtutorial/evaluation/models.py
@@ -1,23 +1,32 @@
 from django.db import models
+from users.models import User
 
 class Eval(models.Model):
     user_id = models.IntegerField()
     user_email = models.EmailField()
-    nombre_emp=models.CharField(max_length=100)
-    rut_emp=models.CharField(max_length=50)
-    sup_email= models.EmailField()
-    nombre_sup=models.CharField(max_length=100)
-    rut_sup=models.CharField(max_length=100)
-    profesor=models.CharField(max_length=100)
-    resumen = models.CharField(max_length=100)
-    fecha_ini=models.DateTimeField()
-    fecha_ter=models.DateTimeField()
     estado = models.CharField(max_length=20)  # Terminado o Pendiente
-    evaluacion1 = models.CharField(max_length=1000)
-    nota1= models.DecimalField(max_digits=3,decimal_places=2)
-    evaluacion2 = models.CharField(max_length=1000)
-    nota2= models.DecimalField(max_digits=3,decimal_places=2)
-    evaluacion3 = models.CharField(max_length=1000)
-    nota3= models.DecimalField(max_digits=3,decimal_places=2)
-    paso=models.IntegerField()
+    
+    # Nuevas columnas
+    fecha_ini = models.DateTimeField(null=True, blank=True)
+    fecha_ter = models.DateTimeField(null=True, blank=True)
+    nombre_emp = models.CharField(max_length=100, null=True, blank=True)
+    rut_emp = models.CharField(max_length=50, null=True, blank=True)
+    sup_email = models.EmailField(null=True, blank=True)
+    nombre_sup = models.CharField(max_length=100, null=True, blank=True)
+    rut_sup = models.CharField(max_length=100, null=True, blank=True)
+    profesor = models.CharField(max_length=100, null=True, blank=True)
+    resumen = models.CharField(max_length=100, null=True, blank=True)
+    evaluacion1 = models.CharField(max_length=1000, null=True, blank=True)
+    nota1 = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    evaluacion2 = models.CharField(max_length=1000, null=True, blank=True)
+    nota2 = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    evaluacion3 = models.CharField(max_length=1000, null=True, blank=True)
+    nota3 = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    paso = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.pasantia} - {self.user_email}"
+
+
+
 
