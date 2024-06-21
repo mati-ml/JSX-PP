f3865bd76798610c64ca3d96187d9f01f9be67b7
Matias Muller <mmullerlanas@gmail.com>
2024-06-20 23:04:24 -0400

    Dcokerizar Django

--- proyecto-final/api/authtutorial/authtutorial/settings.py
+++ proyecto-final/api/authtutorial/authtutorial/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'postgres', #os.environ.get('PG_DB', 'postgres'),
         'USER':'postgres', #os.environ.get('PG_USER', 'postgres'),
         'PASSWORD': 'postgres',#os.environ.get('PG_PASSWORD', 'postgres'),
-        'HOST': 'localhost',#os.environ.get('PG_HOST', '0.0.0.0),
+        'HOST': 'db2',#os.environ.get('PG_HOST', '0.0.0.0),
         'PORT': '5432' #os.environ.get('PG_PORT', '5432'),
     }
 }
