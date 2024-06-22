067e8ff4f5c3c6742dda944c08ce2d1fe70d1ce9
Matias Muller <mmullerlanas@gmail.com>
2024-06-22 19:20:25 -0400

    Merge hecho funcional

--- proyecto-final/src/i18n.js
+++ proyecto-final/src/i18n.js
@@ -2,20 +2,20 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 // Define available languages (replace with your languages)
-const languages = ['en', 'es']; // English and Spanish
+//const languages = ['en']//, 'es']; // English and Spanish
 
 // Import translation files (explained later)
 import translationEN from './translations/en.json';
-import translationES from './translations/es.json';
+//import translationES from './translations/es.json';
 
 i18n
   .use(initReactI18next) // Initializes i18next with react-i18next
   .init({
     resources: {
       en: { translation: translationEN },
-      es: { translation: translationES },
+      //es: { translation: translationES },
     },
-    lng: 'en', // Set default language (can be 'auto' for browser detection)
+    lng: 'auto', // Set default language (can be 'auto' for browser detection)
     interpolation: {
       escapeValue: false, // This ensures raw HTML in translations is displayed correctly
     },
